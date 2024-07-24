b3ac60b896fb8ea3ec5a70fc676676bfb5e91cf1
MelanyMFM <142820807+MelanyMFM@users.noreply.github.com>
2024-07-24 00:12:22 +0000

    cms

---
+++ cmas.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define sq(a) (a)*(a)
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define pb(a) push_back(a)
+#define endl "\n"
+
+void imprimir(vector<int> arr){
+    for (auto val : arr){
+        cout << val << " ";
+    }
+}
+
+
+void solution(){
+    
+}
+
+
+
+int main(){   
+    int t;
+    cin >> t;
+    while(t--){
+        solution();
+    }
+
+    return 0;
+}
