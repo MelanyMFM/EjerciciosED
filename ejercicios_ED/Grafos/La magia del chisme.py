e345078bb652faa29878a83051e75f045023e725
MelanyMFM <142820807+MelanyMFM@users.noreply.github.com>
2024-09-15 04:18:20 +0000

    avance

--- ejercicios_ED/Grafos/La magia del chisme.py
+++ ejercicios_ED/Grafos/La magia del chisme.py
@@ -1,29 +1,41 @@
+from collections import deque
+
+
+
 class Persona:
     def __init__(self, id):
         self.id = id
         self.amigos = None
-        self.contoChisme = 0
         self.escuchoChisme = False
 
 
 personas = [Persona(i) for i in range(int(input()))]
 
+def contadorAmigo(persona):
+    global personas
+    cont = 0
+    for i in range(len(persona.amigos)):
+        if personas[i].escuchoChisme == False: cont += 1
 
-for i in range(len(personas)):
+    return cont
+
+
+
+for j in range(len(personas)):
     temp = list(map(int, input().split()))
-    if temp[0] == -1: temp = []
-    if temp != []: 
-        for j in range(len(temp)):
-            personId = temp[j]
-            temp[j] = personas[personId]
+    if temp[0] == -1: personas[j].amigos = []
+    else:
+        temp = [personas[temp[i]] for i in range(len(temp))]
+        personas[j].amigos = temp
 
-    personas[i].amigos = temp
+empezo = 0
 
-casos = list(map(int, input().split(", ")))
+personas[empezo].escuchoChisme = True
+q = deque()
+q.append(personas[empezo])
 
-for originoChisme in casos:
-    
-
-
-
-
+dia = 1
+contDia = len(personas[empezo].amigos)
+nextDia
+while len(q) > 0:
+    u = q.popleft()
