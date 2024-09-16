b6052eb7252e9c7a76963c16dd697cc918cfe053
Melany <mefrancom@unal.edu.co>
2024-09-15 21:56:53 -0500

    avances malos

--- ejercicios_ED/Grafos/La magia del chisme.py
+++ ejercicios_ED/Grafos/La magia del chisme.py
@@ -6,18 +6,12 @@ class Persona:
         self.amigos = None
         self.conto_chisme = 0
         self.escucho_chisme = False
+        self.dia = 0
+        self.segunda = False
 
 
 personas = [Persona(i) for i in range(int(input()))]
 
-
-def amigos_no(persona):
-    global personas
-    cont = 0
-    for i in range(len(persona.amigos)):
-        if personas[persona.amigos[i]].escucho_chisme == False: cont += 1
-
-    return cont
 
 
 for i in range(len(personas)):
@@ -30,19 +24,55 @@ for i in range(len(personas)):
 
     personas[i].amigos = temp
 
-originoChisme = 0
-q = deque()
-q.append(personas[originoChisme])
-personas[originoChisme].escucho_chisme = True
-contDia, contNextDia = 1, 0
 
-while len(q) > 0:
 
-    u = q.popleft()
+popo = list(map(int, input().split(", ")))
+for kk in popo:
+    
+    for ola in personas:
+        ola.segunda = False
+        ola.escucho_chisme = False
 
-    for v in u.amigos:
-        if v.escucho_chisme == False:
-            v.escucho_chisme = True
-            q.append(v)
+    originoChisme = kk
+    q = deque()
+    q.append(personas[originoChisme])
+    personas[originoChisme].escucho_chisme = True
 
+
+    while len(q) > 0:
+
+        u = q.popleft()
+
+        for v in u.amigos:
+            if v.escucho_chisme == False:
+                v.escucho_chisme = True
+                v.dia = u.dia + 1
+                q.append(v)
+
+    dias = {}
+    q = deque()
+    q.append(personas[originoChisme])
+    personas[originoChisme].segunda = True
+
+    while len(q) > 0:
+        u = q.popleft()
+
+        for v in u.amigos:
+            if v.segunda == False:
+                v.segunda = True
+
+                if v.dia not in dias: dias[v.dia] = 1
+                else: dias[v.dia] += 1
+                q.append(v)
+
+
+    max = 0
+    cant = 0
+    for i in dias:
+        if cant < dias[i]:
+            cant = dias[i]
+            max = i
+
+    if max != 0 and cant != 0: print(max, cant)
+    else: print(0)
 
