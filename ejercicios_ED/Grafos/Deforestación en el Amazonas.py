15650e1de3a724bf9eead755b050398e6c4c14cc
Melany <mefrancom@unal.edu.co>
2024-09-16 10:17:16 -0500

    listo

--- ejercicios_ED/Grafos/Deforestación en el Amazonas.py
+++ ejercicios_ED/Grafos/Deforestación en el Amazonas.py
@@ -1,16 +1,45 @@
-a, b = input().split()
-a, b = int(a), int(b)
+from collections import deque
+for _ in range(int(input())):
+    a, b = input().split()  #rows - columnas
+    a, b = int(a), int(b)
 
-def vecinos(i, j):
-    ies = []
-    js = []
-    if i == 0: #arriba del todo
-        #voy a guardar todas las coordenadas i, j y luego hare una lista de tuplas de cada vecino i, j
+    def vecinos(i, j):
+        global a, b
+        coords = deque()
+        if j != 0:  coords.append((i, j-1))
+
+        if i != 0: coords.append((i-1, j))
+
+        if j != b-1: coords.append((i, j+1))
+
+        if i != a-1: coords.append((i+1, j))
+
+        return coords
 
 
 
+    amazonas = [ list(input()) for _ in range(a)]
 
-amazonas = [ list(input()) for _ in range(a)]
+    majino = 0
 
-for i in amazonas:
-    print(i)
+    for i in range(a):
+        for j in range(b):
+
+            if amazonas[i][j] == "X":
+                s = deque()
+                s.append((i, j))
+                cont = 0
+
+                while len(s) > 0:
+                    u = s.pop()
+                    if amazonas[u[0]][u[1]] == "X":
+                        amazonas[u[0]][u[1]] = "ya"
+                        cont += 1
+
+                        for kk in vecinos(u[0], u[1]):
+                            if amazonas[kk[0]][kk[1]] == "X":
+                                s.append(kk)
+
+                majino = max(majino, cont)
+
+    print(majino)
