da3cb496eff03965d1b979308ca3960768d5ce8c
Melany <mefrancom@unal.edu.co>
2024-09-15 23:35:40 -0500

    adelanto

---
+++ ejercicios_ED/Grafos/Deforestación en el Amazonas.py
@@ -0,0 +1,16 @@
+a, b = input().split()
+a, b = int(a), int(b)
+
+def vecinos(i, j):
+    ies = []
+    js = []
+    if i == 0: #arriba del todo
+        #voy a guardar todas las coordenadas i, j y luego hare una lista de tuplas de cada vecino i, j
+
+
+
+
+amazonas = [ list(input()) for _ in range(a)]
+
+for i in amazonas:
+    print(i)
