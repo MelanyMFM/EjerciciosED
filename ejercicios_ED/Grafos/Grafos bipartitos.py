2ad87725dd58ae36b2734c6ec0cf74f51d776571
Melany <mefrancom@unal.edu.co>
2024-09-15 23:19:24 -0500

    termonado

--- ejercicios_ED/Grafos/Grafos bipartitos.py
+++ ejercicios_ED/Grafos/Grafos bipartitos.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Nodo:
     def __init__(self, id):
         self.color = None
@@ -6,16 +8,49 @@ class Nodo:
         self.aristas = []
 
 
-n, m  = input().split() #nodos, aristas
-n, m = int(n), int(m)
+def contrario(color):
+    return "dabadaba" if color == "dubidubi" else "dubidubi"
 
-grafo = [Nodo(i) for i in range(n)]
+for _ in range(int(input())):
+    n, m  = input().split() #nodos, aristas
+    n, m = int(n), int(m)
 
-for _ in range(m):
-    a, b = input().split(", ")
-    a, b = int(a), int(b)
+    grafo = [Nodo(i) for i in range(1, n+1)]
+    grafo.insert(0, None)
 
-    grafo[a].aristas.append(grafo[b])
-    grafo[b].aristas.append(grafo[a])
+    for _ in range(m):
+        a, b = input().split(", ")
+        a, b = int(a), int(b)
+
+        grafo[a].aristas.append(grafo[b])
+        grafo[b].aristas.append(grafo[a])
+
+    bipartito = True
+    siga = True
+
+    for i in grafo:
+        if i == None: continue
+        if i.visitado == True: continue 
+
+        q = deque()
+        i.visitado = True
+        i.color = "dubidubi"
+        q.append(i)
+
+        while len(q) > 0 and siga:
+            u = q.popleft()
+
+            for j in u.aristas:
+
+                if j.visitado == False:
+                    j.visitado = True
+                    j.color = contrario(u.color)
+                    q.append(j)
+
+                else:
+                    if j.color != contrario(u.color): 
+                        bipartito = False
+                        siga = False
 
 
+    print("bipartito" if bipartito else "no bipartito")
